a7f4ddefa2ab44c5095d9c1d791db70e35f77070
Sekara Dileeban <140878415+Dileeban1108@users.noreply.github.com>
2024-03-03 14:44:02 +0530

    Update Dashboard.js

--- src/pages/user/Dashboard.js
+++ src/pages/user/Dashboard.js
@@ -5,14 +5,14 @@ import { useAuth } from "../../context/auth";
 const Dashboard = () => {
   const [auth] = useAuth();
   return (
-    <Layout title={"Dashboard - Ecommerce App"}>
-      <div className="container-flui m-3 p-3 dashboard">
+    <Layout title={"Dashboard - VI Solutions"}>
+      <div className="container-flui m-3 p-3 dashboard" >
         <div className="row">
           <div className="col-md-3">
             <UserMenu />
           </div>
-          <div className="col-md-9">
-            <div className="card w-75 p-3">
+          <div className="col-md-9" >
+            <div className="card w-75 p-3" >
               <h3>{auth?.user?.name}</h3>
               <h3>{auth?.user?.email}</h3>
               <h3>{auth?.user?.address}</h3>
